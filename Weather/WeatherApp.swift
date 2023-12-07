64c13eba235b8244a7f518dc6aa6919434897df8
tksohlt <kimisami31@gmail.com>
2023-12-07 13:45:38 -0600

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Bitala1_4 on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
